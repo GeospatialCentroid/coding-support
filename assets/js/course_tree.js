6342dfdc03edd6e567ec8cd4a032df54b3438088
AndrewZimbelman <andrew.zimbelman@colostate.edu>
2026-08-16 20:31:15 -0600

    modified dimensions of course tree pane

--- assets/js/course_tree.js
+++ assets/js/course_tree.js
@@ -81,7 +81,7 @@ function renderTree(rootNode) {
   
   let height = Math.max(400, totalNodes * 28); 
   
-  const margin = { top: 40, right: 300, bottom: 40, left: 100 };
+  const margin = { top: 40, right: 250, bottom: 40, left: 75 };
   const treeWidth = width - margin.left - margin.right;
   const treeHeight = height - margin.top - margin.bottom;
 
@@ -148,7 +148,7 @@ function renderTree(rootNode) {
   // Truncate text if it exceeds a character limit to protect layout boundaries
   textElement.text(d => {
     const name = d.data.name;
-    const maxLength = 50; //
+    const maxLength = 35;
     return name.length > maxLength ? name.substring(0, maxLength) + "..." : name;
   });
 
